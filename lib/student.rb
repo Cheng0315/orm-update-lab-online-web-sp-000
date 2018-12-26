72a7d1f43f7b0a1202c85f20caa084321737bbff
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 01:50:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,4 +47,15 @@ class Student
     student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+  end
+
 end
