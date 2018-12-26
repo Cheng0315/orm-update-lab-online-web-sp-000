186a8682e91ae781a9f8d9f34a09dcb332db0af4
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 02:15:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,14 +26,18 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+        SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+        DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create(name, grade)
@@ -62,5 +66,4 @@ class Student
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-
 end
