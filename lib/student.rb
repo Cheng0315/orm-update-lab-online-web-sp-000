fea50d1d1b34a2e6bd4b632c2642f1eb0720a18c
Cheng Vue <cvu3@ucsc.edu>
2018-12-26 01:00:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade, :id
 
-  def initialize(id = nil, name, grade)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
@@ -35,5 +35,9 @@ class Student
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+
+  def self.create(hash)
+    student = Student.new(hash[:name], hash[:grade])
+  end 
 
 end
